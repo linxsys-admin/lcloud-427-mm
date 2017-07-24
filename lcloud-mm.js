4dabbc8d32d62011e790a6212d4fa724c41ea593
Modest Machnicki <mmachnicki@gmail.com>
2017-07-24 14:26:18 +0200

    Delete by regex command.

--- lcloud-mm.js
+++ lcloud-mm.js
@@ -22,7 +22,7 @@ program
 program
   .command('list')
   .description('list all files in an S3 Bucket')
-  .option("-f, --filter", "regex filter")
+  .option("-f, --filter [filter]", "regex filter")
   .action(filter => {
     commands.list(s3, filter);
   });
@@ -37,6 +37,17 @@ program
       console.error('file/target not defined');
     }
   });
+
+  program
+    .command('delete [regex]')
+    .description('delete all files matching a regex from a bucket')
+    .action((regex) => {
+      if (regex) {
+        commands.delete(s3, regex);
+      } else {
+        console.error('regex not defined');
+      }
+    });
 
 
 program.parse(process.argv);
